c8967de10b037a5b8fb4f67b0b9b4242090cf7d9
94ju <janithherath94@gmail.com>
2019-05-09 15:52:09 +0530

    all the endpoints fixed

---
+++ index.js
@@ -0,0 +1,24 @@
+const express=require('express');
+const app=express();
+const fetch =require('node-fetch');
+
+app.get('/statetocode/:state',async(req,res)=>{
+   
+    const state=req.params.state;
+    const statelist = await fetch("http://localhost:3000/getstate");
+    const jsonstatelist =await statelist.json();
+    const code =jsonstatelist.find(element => element.name===state).abbreviation;
+    res.send({code:code});
+
+});
+
+app.get('/codetostate/:code',async(req,res)=>{
+    const code=req.params.code;
+    const codelist = await fetch("http://localhost:3000/getcode");
+    const jsoncodelist = await codelist.json();
+    const state =jsoncodelist[code];
+    res.send({state:state});
+});
+
+
+app.listen(3001);
